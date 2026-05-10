5da884b794dca01282e2d2e4baf064c04316be74
YARRABATI VENKATESH NAIDU <69239877+Venky098@users.noreply.github.com>
2026-05-10 22:39:52 +0530

    Rename evensquare.java to Evensquare.java

--- src/main/Numbers/Evensquare.java
+++ src/main/Numbers/Evensquare.java
@@ -1,7 +1,7 @@
 
 import java.util.*;
 import java.util.stream.*;
-class Main {
+class Evensquare {
     public static void main(String[] args) {
        List<Integer> num = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
        
