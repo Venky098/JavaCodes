4c861c748796dfe2196fd8702d5b91c2a0d24d3b
YARRABATI VENKATESH NAIDU <69239877+Venky098@users.noreply.github.com>
2026-05-10 22:39:21 +0530

    Add evensquare.java to filter and square even numbers
    Implement a Java program to filter even numbers and compute their squares.

---
+++ src/main/Numbers/evensquare.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+import java.util.stream.*;
+class Main {
+    public static void main(String[] args) {
+       List<Integer> num = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+       
+       List<Integer> even = num.stream().filter(n->n%2==0).toList();
+       
+       List<Integer> evensq = even.stream().map(n->n*n).collect(Collectors.toList());
+       System.out.println(even.toString());
+       System.out.println(evensq.toString());
+}
+
+}
