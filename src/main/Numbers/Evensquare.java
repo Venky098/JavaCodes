ab1c02833e32e804c99f2c4192d4d9939f3e1d2b
YARRABATI VENKATESH NAIDU <69239877+Venky098@users.noreply.github.com>
2026-05-10 22:43:23 +0530

    Implement even number filtering using bitwise AND
    Added an alternative method to filter even numbers using bitwise AND.

--- src/main/Numbers/Evensquare.java
+++ src/main/Numbers/Evensquare.java
@@ -6,9 +6,12 @@ class Evensquare {
        List<Integer> num = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
        
        List<Integer> even = num.stream().filter(n->n%2==0).toList();
-       
+
+
+         List<Integer> evenusingand = num.stream().filter(n->(n&1)==0).toList();
        List<Integer> evensq = even.stream().map(n->n*n).collect(Collectors.toList());
        System.out.println(even.toString());
+        System.out.println(evenusingand.toString());
        System.out.println(evensq.toString());
 }
 
